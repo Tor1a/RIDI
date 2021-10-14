cccf310a04307710f822290bb106d2ba4efc5036
Tor1a <se_o_ul@naver.com>
2021-10-15 04:14:40 +0900

    qna 작업

--- RIDI/src/main/java/ridi/controller/QnaController.java
+++ RIDI/src/main/java/ridi/controller/QnaController.java
@@ -1,11 +1,26 @@
 package ridi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import ridi.model.qna.QnaDao;
+import ridi.model.qna.QnaDto;
 
 @Controller
 @Slf4j
 public class QnaController {
 
+	@Autowired
+	QnaDao qnaDao;
+	
+	@Autowired
+	QnaDto qnaDto;
+	
+	@RequestMapping("QnaWriteForm.do")
+	public String qnaWriteForm() {
+		return "qna/qna_Write";
+				
+	}
 }
--- RIDI/src/main/java/ridi/model/qna/QnaDao.java
+++ RIDI/src/main/java/ridi/model/qna/QnaDao.java
@@ -1,12 +1,45 @@
 package ridi.model.qna;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import lombok.extern.slf4j.Slf4j;
 import ridi.model.member.MemberDao;
 
 @Repository
-@Slf4j
 public class QnaDao {
-
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	//session factory 받기
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	public List<QnaDto> getAllList(int start, int end){
+		HashMap<String, Integer> page = new HashMap<String, Integer>();
+		page.put("start", start);
+		page.put("end", end);
+		
+		List<QnaDto> qnaList = null;
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		qnaList = sqlSession.selectList("getAllList",page);
+		sqlSession.close();
+		return qnaList;
+	}
+	public int insertQna(QnaDto qnaDto) {
+		int result = 0 ;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		result =  sqlSession.insert("insertQna",qnaDto);
+		sqlSession.commit();
+		sqlSession.close();
+		
+		return result;
+	}
 }
