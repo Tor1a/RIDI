b450d83ccaee414d2e0e9cfb6cada822562688d2
lkoosss <lkoosss@gmail.com>
2021-10-26 22:16:14 +0900

    세진님 작업적용

--- RIDI/src/main/webapp/WEB-INF/views/book/book_Comic.jsp
+++ RIDI/src/main/webapp/WEB-INF/views/book/book_Comic.jsp
@@ -1,6 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ include file="../include/header.jsp"%>
-
+<div class="headerCategoryLine">
+	<nav class="headerCategoryInner">
+		<ul class="headerCategory">
+			<li><i class="fas fa-caret-right"></i></li>
+			<li><a href="MoneyBookForm.do">재테크</a></li>
+			<li><a href="ComicBookForm.do">만화</a></li>
+			<li><a href="EssayBookForm.do">에세이</a></li>
+		</ul>
+	</nav>
+</div>
 
 <%@ include file="../include/footer.jsp"%>
--- RIDI/src/main/webapp/resources/js/main.js
+++ RIDI/src/main/webapp/resources/js/main.js
@@ -3,28 +3,28 @@ let clickedItem;
 let plusNum;
 //$(document).ready(function(){ $(".slick-sample").slick({ }); }); 
 /*var $opts = 
-           { 
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite : true,
-                autoplay: true,
-                autoplaySpeed: 1000,
-                arrows: true,
-                prevArrow: $('#aro1_prev'),
-                nextArrow: $('#aro1_next')
-        	};*/
+		   { 
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite : true,
+				autoplay: true,
+				autoplaySpeed: 1000,
+				arrows: true,
+				prevArrow: $('#aro1_prev'),
+				nextArrow: $('#aro1_next')
+			};*/
 
 /* 메인 슬라이더 */
-$(document).ready(function(){
+$(document).ready(function() {
 	$.ajax({
-		type : 'GET',
-		url:"BookJsonSliderList.do",
-		dataType : 'json',
-		success:function(resultData){
+		type: 'GET',
+		url: "BookJsonSliderList.do",
+		dataType: 'json',
+		success: function(resultData) {
 			//console.log(resultData.bookList);
 			const bookList = resultData.bookList;
 			$(".main_slider").append(`<ul class="list"></ul>`);
-			$.each(bookList, function(i, item){
+			$.each(bookList, function(i, item) {
 				$(".main_slider .list").append(`	<li>	
 														<a href="BookInfo.do?no=${item.no}">
 															<img src="${item.book_Image}">
@@ -33,33 +33,33 @@ $(document).ready(function(){
 											`);
 			});
 			$(".main_slider").removeClass("slick-initialized slick-slider");
-			
+
 			$(".main_slider .list").slick({
-	            slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite : true,
-                autoplay: true,
-                autoplaySpeed: 1000,
-                arrows: false,
-                dots: true
-	         });
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				autoplay: true,
+				autoplaySpeed: 1000,
+				arrows: false,
+				dots: true
+			});
 		}
 	});
- }); 
+});
 
 
 /* 신작 */
 
-$(document).ready(function(){
+$(document).ready(function() {
 	$.ajax({
-		type : 'GET',
-		url:"NewBookJsonList.do",
-		dataType : 'json',
-		success:function(resultData){
+		type: 'GET',
+		url: "NewBookJsonList.do",
+		dataType: 'json',
+		success: function(resultData) {
 			//console.log(resultData.newBookList);
 			const newBookList = resultData.newBookList;
 			//$(".newBook_Slide").append(`<div class="newBookList"></div>`);
-			$.each(newBookList, function(i, item){
+			$.each(newBookList, function(i, item) {
 				$(".newBook_Slide").append(`
 													<div class="newBookList">
 														<a href="BookInfo.do?no=${item.no}">
@@ -72,33 +72,33 @@ $(document).ready(function(){
 													</div>
 											`);
 			});
-			 $(".newBook_Slide").removeClass("slick-initialized slick-slider");
-			
-	         $(".newBook_Slide").slick({
-	            dots: false,
-                infinite: true,
-                speed: 300,
-                slidesToShow: 6,
-                slidesToScroll: 6,
-                arrows: true
-	         });	
+			$(".newBook_Slide").removeClass("slick-initialized slick-slider");
+
+			$(".newBook_Slide").slick({
+				dots: false,
+				infinite: true,
+				speed: 300,
+				slidesToShow: 6,
+				slidesToScroll: 6,
+				arrows: true
+			});
 		}
-	});	
-	
-}); 
+	});
+
+});
 
 
 /* 재태크페이지 메인 베스트 슬라이더 출력(미완) */
-$(document).ready(function(){
+$(document).ready(function() {
 	$.ajax({
-		type : 'GET',
-		url:"MoneyBestBookJsonList.do",
-		dataType : 'json',
-		success:function(resultData){
+		type: 'GET',
+		url: "MoneyBestBookJsonList.do",
+		dataType: 'json',
+		success: function(resultData) {
 			//console.log(resultData.moneyBestBookList);
 			const moneyBestBookList = resultData.moneyBestBookList;
 			$(".best_slider").append(`<ul class="list"></ul>`);
-			$.each(moneyBestBookList, function(i, item){
+			$.each(moneyBestBookList, function(i, item) {
 				$(".best_slider .list").append(`	<li>	
 														<a href="BookInfo.do?no=${item.no}">
 															<img src="${item.book_Image}">
@@ -107,19 +107,19 @@ $(document).ready(function(){
 											`);
 			});
 			$(".best_slider").removeClass("slick-initialized slick-slider");
-			
+
 			$(".best_slider .list").slick({
-	            slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite : true,
-                autoplay: true,
-                autoplaySpeed: 1000,
-                arrows: false,
-                dots: true
-	         });
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				autoplay: true,
+				autoplaySpeed: 1000,
+				arrows: false,
+				dots: true
+			});
 		}
 	});
- }); 
+});
 /* 재태크페이지 지금시간페이지 출력(미완) */
 
 /* 현재 시간 출력 */
@@ -130,45 +130,45 @@ let time = hour + "시 " + minutes + "분";
 console.log(time);
 
 $.ajax({
-		type : 'GET',
-		url:"MoneyTimeBookJsonList.do",
-		dataType : 'json',
-		success:function(resultData){
-			console.log(resultData.moneyTimeBookList);
-			const moneyTimeBookList = resultData.moneyTimeBookList;
-			$(".time").append(time);
-			$.each(moneyTimeBookList, function(i, item){
-				$(".gridMoney").append(`	<li class="img_Box${i+1}">
+	type: 'GET',
+	url: "MoneyTimeBookJsonList.do",
+	dataType: 'json',
+	success: function(resultData) {
+		console.log(resultData.moneyTimeBookList);
+		const moneyTimeBookList = resultData.moneyTimeBookList;
+		$(".time").append(time);
+		$.each(moneyTimeBookList, function(i, item) {
+			$(".gridMoney").append(`	<li class="img_Box${i + 1}">
 				                         <a href="BookInfo.do?no=${item.no}">
 				                             <div>
 				                                 <img src="${item.book_Image}">
 				                             </div>
 				                         </a>
 				                         <div>
-				                             <h4>${i+1}</h4>
+				                             <h4>${i + 1}</h4>
 				                             <div>
 				                                 <a href="BookInfo.do?no=${item.no}">${item.book_Name}</a>
 				                                 <span>${item.author}</span>
 				                             </div>
 				                         </div>
 				                     </li>`
-									);
-			});
-			
-		}
-	});
+			);
+		});
+
+	}
+});
 
 /* 재태크페이지 리디 추천 슬라이더 출력(미완) */
 
-$(document).ready(function(){
+$(document).ready(function() {
 	$.ajax({
-		type : 'GET',
-		url:"MoneyRecomBookJsonList.do",
-		dataType : 'json',
-		success:function(resultData){
+		type: 'GET',
+		url: "MoneyRecomBookJsonList.do",
+		dataType: 'json',
+		success: function(resultData) {
 			console.log(resultData.moneyRecomBookList);
 			const moneyRecomBookList = resultData.moneyRecomBookList;
-			$.each(moneyRecomBookList, function(i, item){
+			$.each(moneyRecomBookList, function(i, item) {
 				$(".recomBook_Slide").append(`
 													<div class="recomBookList">
 														<a href="BookInfo.do?no=${item.no}">
@@ -181,40 +181,40 @@ $(document).ready(function(){
 													</div>
 											`);
 			});
-			 $(".recomBook_Slide").removeClass("slick-initialized slick-slider");
-			
-	         $(".recomBook_Slide").slick({
-	            dots: false,
-                infinite: true,
-                speed: 300,
-                slidesToShow: 6,
-                slidesToScroll: 6,
-                arrows: true
-	         });	
+			$(".recomBook_Slide").removeClass("slick-initialized slick-slider");
+
+			$(".recomBook_Slide").slick({
+				dots: false,
+				infinite: true,
+				speed: 300,
+				slidesToShow: 6,
+				slidesToScroll: 6,
+				arrows: true
+			});
 		}
-	});	
-	
-}); 
+	});
+
+});
 
 
 //오성식 추가
 //베스트 셀러를 출력한다.
-$(document).ready(function(){
+$(document).ready(function() {
 	$.ajax({
-		type:"GET",
-		url:"BestSellerJsonList.do",
-		dataType:"json",
-		success:function(result){
+		type: "GET",
+		url: "BestSellerJsonList.do",
+		dataType: "json",
+		success: function(result) {
 			const bestSellerList = result.bestSellerList;
-			$.each(bestSellerList,function(i,item){
-				$(".grid").append(`<li class="img_Box${i+1}">
+			$.each(bestSellerList, function(i, item) {
+				$(".grid").append(`<li class="img_Box${i + 1}">
                          <a href="BookInfo.do?no=${item.no}">
                              <div>
                                  <img src="${item.book_Image}" alt="">
                              </div>
                          </a>
                          <div>
-                             <h4>${i+1}</h4>
+                             <h4>${i + 1}</h4>
                              <div>
                                  <a href="BookInfo.do?no=${item.no}">${item.book_Name}</a>
                                  <span>${item.author}</span>
@@ -222,23 +222,22 @@ $(document).ready(function(){
                              </div>
                          </div>
                      </li>`);
-				for(var x=1; x<=5; x++){
-					if(x<=`${item.star_Rating}`){
-						$(`.grid .img_Box${i+1} > div > div span:nth-child(3)`).append("★");
-						$(`.grid .img_Box${i+1} > div > div span:nth-child(3)`).attr("style","font-size:20px; color:gold;");
+				for (var x = 1; x <= 5; x++) {
+					if (x <= `${item.star_Rating}`) {
+						$(`.grid .img_Box${i + 1} > div > div span:nth-child(3)`).append("★");
+						$(`.grid .img_Box${i + 1} > div > div span:nth-child(3)`).attr("style", "font-size:20px; color:gold;");
 					} else {
-						$(`.grid .img_Box${i+1} > div > div span:nth-child(3)`).append("☆");
-						$(`.grid .img_Box${i+1} > div > div span:nth-child(3)`).attr("style","font-size:20px; color:gold;");
+						$(`.grid .img_Box${i + 1} > div > div span:nth-child(3)`).append("☆");
+						$(`.grid .img_Box${i + 1} > div > div span:nth-child(3)`).attr("style", "font-size:20px; color:gold;");
 					}
-				}		
+				}
 			});
 		},
-		error:function(){
+		error: function() {
 			alert("실패함");
 		}
 	})
 })
-
 
 
 
